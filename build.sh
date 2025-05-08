1b896ec31b8ac0fdd89ae60694783546bb0c4698
Dimitri POMIER <dpomier@gaillare.tech>
2025-05-08 20:39:02 +0200

    script portability

--- build.sh
+++ build.sh
@@ -1,1 +1,4 @@
-docker build --pull -t haxe:4.3.6-gllr .
+HERE="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+echo "Build image haxe:4.3.6-gllr"
+docker build --pull -t haxe:4.3.6-gllr $HERE
