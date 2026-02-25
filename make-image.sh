f93f60bf287b2768ec2ac9ea65f6dc9d899070b5
dpomier <dpomier@gaillare.tech>
2026-02-25 14:28:14 +0100

    allow caching the base image to build offline

--- make-image.sh
+++ make-image.sh
@@ -1,4 +1,8 @@
 HERE="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
+echo "Pull base image to allow offline caching"
+OS=debian:12.8-slim
+docker image pull $OS
+
 echo "Build image haxe:4.3.6-gllr"
-docker build --pull -t haxe:4.3.6-gllr $HERE
+docker build --pull -t haxe:4.3.6-gllr --build-arg os=$OS $HERE
