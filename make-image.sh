cbc3bcd98299f3669e6125d0ee52c623e2117c2e
dpomier <dpomier@gaillare.tech>
2026-02-26 18:37:18 +0100

    rename image

--- make-image.sh
+++ make-image.sh
@@ -4,5 +4,5 @@ echo "Pull base image to allow offline caching"
 OS=debian:12.8-slim
 docker image pull $OS
 
-echo "Build image haxe:4.3.6-gllr"
-docker build --pull -t haxe:4.3.6-gllr --build-arg os=$OS $HERE
+echo "Build image dhaxe:4.3.6"
+docker build --pull -t dhaxe:4.3.6 --build-arg os=$OS $HERE
